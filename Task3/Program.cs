18dc9e85e9ac9992abd5de6528d565ac2a45e68a
Nikolay.Reshetov <275480217@mail.ru>
2023-02-13 04:22:45 +0300

    Задача 3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int nEven = 2;
+
+if (N>1)
+{
+	Console.Write("Четные числа между 1 и N: ");
+	while (nEven<=N)
+	{
+		Console.Write(nEven + ", ");
+		nEven +=2;
+	}
+}
+else
+	Console.WriteLine("Четные числа между 1 и N ОТСУТСТВУЮТ");
